5b38918494c431cc4035ee04af55b1418db34506
ioanbujdei <ioan.bujdei97@e-uvt.ro>
2026-05-17 21:55:15 +0300

    Update types.ts

--- utils/types.ts
+++ utils/types.ts
@@ -6,10 +6,17 @@ export interface ImageProps {
   public_id: string;
   format: string;
   blurDataUrl?: string;
-  // Add these new fields
-  artist?: string;      
-  year?: string;        
-  description?: string; 
+  // Define the allowed metadata fields from Cloudinary context
+  context?: {
+    artist?: string;
+    location?: string;
+    contact?: string;
+    title?: string;
+    medium?: string;
+    size?: string;
+    status?: string;
+    description?: string;
+  };
 }
 
 export interface SharedModalProps {
