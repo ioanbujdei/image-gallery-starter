ea9bb46cbc6801f0aec9c3e2a7f0f479981166b4
ioanbujdei <ioan.bujdei97@e-uvt.ro>
2026-05-24 20:22:46 +0300

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,20 +8,20 @@ class MyDocument extends Document {
           <link rel="icon" href="/favicon.ico" />
           <meta
             name="description"
-            content="Alpha.1 Fine Art Exhibition Gallery."
+            content="Alpha.1 Exhibition"
           />
+          {/* Replace this URL with your preferred thumbnail link */}
+          <meta property="og:image" content="https://image-gallery-starter-phi-liard.vercel.app/_next/image?url=%2Falpha-logo.png&w=640&q=75" />
           <meta property="og:site_name" content="Alpha.1 Exhibition" />
           <meta
             property="og:description"
-            content="Explore the Alpha.1 Exhibition gallery."
+            content="Explore the Alpha.1 Exhibition."
           />
           <meta property="og:title" content="Alpha.1 Exhibition" />
           <meta name="twitter:card" content="summary_large_image" />
+          {/* Replace this URL with your preferred thumbnail link */}
+          <meta name="twitter:image" content="https://image-gallery-starter-phi-liard.vercel.app/_next/image?url=%2Falpha-logo.png&w=640&q=75" />
           <meta name="twitter:title" content="Alpha.1 Exhibition" />
-          <meta
-            name="twitter:description"
-            content="Explore the Alpha.1 Exhibition gallery."
-          />
         </Head>
         <body className="bg-black antialiased">
           <Main />
