e5e7393df6d5ef83d594686f1e130bb52becbd98
ioanbujdei <ioan.bujdei97@e-uvt.ro>
2026-05-24 20:16:56 +0300

    Update generateBlurPlaceholder.ts

--- utils/generateBlurPlaceholder.ts
+++ utils/generateBlurPlaceholder.ts
@@ -1,25 +1,13 @@
-import imagemin from "imagemin";
-import imageminJpegtran from "imagemin-jpegtran";
 import type { ImageProps } from "./types";
 
-const cache = new Map<ImageProps, string>();
-
 export default async function getBase64ImageUrl(
-  image: ImageProps,
+  image: ImageProps
 ): Promise<string> {
-  let url = cache.get(image);
-  if (url) {
-    return url;
-  }
   const response = await fetch(
-    `https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/f_jpg,w_8,q_70/${image.public_id}.${image.format}`,
+    // FIX: v${version} moved AFTER the formatting rules
+    `https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/f_jpg,w_8,q_70/${image.version ? `v${image.version}/` : ''}${image.public_id}.${image.format}`
   );
   const buffer = await response.arrayBuffer();
-  const minified = await imagemin.buffer(Buffer.from(buffer), {
-    plugins: [imageminJpegtran()],
-  });
-
-  url = `data:image/jpeg;base64,${Buffer.from(minified).toString("base64")}`;
-  cache.set(image, url);
-  return url;
+  const data = Buffer.from(buffer).toString("base64");
+  return `data:image/jpeg;base64,${data}`;
 }
