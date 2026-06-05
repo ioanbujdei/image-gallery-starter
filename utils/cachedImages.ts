6ef1f930f243d611bbff54ba388bf7fc866c22b1
ioanbujdei <ioan.bujdei97@e-uvt.ro>
2026-06-05 13:54:28 +0300

    Update cachedImages.ts

--- utils/cachedImages.ts
+++ utils/cachedImages.ts
@@ -6,7 +6,7 @@ export default async function getResults() {
   if (!cachedResults) {
     const fetchedResults = await cloudinary.v2.search
       .expression(`folder:${process.env.CLOUDINARY_FOLDER}/*`)
-      .sort_by("context.order", "asc")
+      .sort_by("public_id", "desc") // Reverted to fix the 400 error
       .max_results(400)
       .with_field('context')
       .execute();
